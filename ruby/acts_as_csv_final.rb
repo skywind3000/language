9cadfad489688de04f83b48b361ebde61ce5f77f
skywind3000 <skywind3000@163.com>
2016-12-11 02:55:07 +0800

    more ruby script

---
+++ ruby/acts_as_csv_final.rb
@@ -0,0 +1,39 @@
+module ActsAsCsv
+	def self.included(base)
+		base.extend ClassMethods
+	end
+	module ClassMethods
+		def acts_as_csv
+			include InstanceMethods
+		end
+	end
+
+	module InstanceMethods
+		def read
+			@csv_contents = []
+			filename = self.class.to_s.downcase + '.txt'
+			file = File.new(filename)
+			@headers = file.gets.chomp.split(', ')
+			file.each do |row|
+				@csv_contents << row.chomp.split(', ')
+			end
+		end
+
+		attr_accessor :headers, :csv_contents
+
+		def initialize
+			read
+		end
+	end
+end
+
+class RubyCsv
+	include ActsAsCsv
+	acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
+
