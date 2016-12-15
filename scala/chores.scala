4bd150e6b651b8b465f7f4e4a70a3848a30d6f28
skywind3000 <skywind3000@163.com>
2016-12-15 19:33:23 +0800

    new operators

---
+++ scala/chores.scala
@@ -0,0 +1,9 @@
+def doChore(chore: String):String = chore match {
+	case "clean dishes" => "scrub, dry"
+	case "cook dinner" => "chop, sizzle"
+	case _ => "whine, complain"
+}
+
+println(doChore("clean dishes"))
+println(doChore("mow lawn"))
+
