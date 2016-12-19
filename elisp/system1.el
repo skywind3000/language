910995f00600e42cedfd1645ab69295584a301fb
skywind3000 <skywind3000@163.com>
2016-12-20 05:09:23 +0800

    new system1.el

---
+++ elisp/system1.el
@@ -0,0 +1,6 @@
+(message "system-type: %s" system-type)
+(message "system-name: %s" (system-name))
+(message "buffer-file-name: %s" buffer-file-name)
+(message "tty-erase-char: %s" tty-erase-char)
+
+
