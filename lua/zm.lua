9abcdc49770f1e92f9f76b7570fa17e03c30e521
skywind3000 <skywind3000@163.com>
2018-03-19 11:58:47 +0800

    update lua demo

--- lua/zm.lua
+++ lua/zm.lua
@@ -6,11 +6,111 @@
 -- Last Modified: 2018/03/19 11:11:47
 --
 --=====================================================================
+local string = string
+local table = table
+local io = io
 local print = print
+local assert = assert
+local type = type
+local pairs = pairs
+local tostring = tostring
 module('zlua')
 
-function data_load(filename)
-	print('filename: '..filename)
+
+-----------------------------------------------------------------------
+-- split string
+-----------------------------------------------------------------------
+function string:split(sSeparator, nMax, bRegexp)
+	assert(sSeparator ~= '')
+	assert(nMax == nil or nMax >= 1)
+	local aRecord = {}
+	if self:len() > 0 then
+		local bPlain = not bRegexp
+		nMax = nMax or -1
+		local nField, nStart = 1, 1
+		local nFirst,nLast = self:find(sSeparator, nStart, bPlain)
+		while nFirst and nMax ~= 0 do
+			aRecord[nField] = self:sub(nStart, nFirst-1)
+			nField = nField+1
+			nStart = nLast+1
+			nFirst,nLast = self:find(sSeparator, nStart, bPlain)
+			nMax = nMax-1
+		end
+		aRecord[nField] = self:sub(nStart)
+	end
+	return aRecord
 end
 
-data_load('abc.txt')
+
+-----------------------------------------------------------------------
+-- print table
+-----------------------------------------------------------------------
+function dump(o)
+	if type(o) == 'table' then
+		local s = '{ '
+		for k,v in pairs(o) do
+			if type(k) ~= 'number' then k = '"'..k..'"' end
+			s = s .. '['..k..'] = ' .. dump(v) .. ','
+		end
+		return s .. '} '
+	else
+		return tostring(o)
+	end
+end
+
+
+-----------------------------------------------------------------------
+-- print table
+-----------------------------------------------------------------------
+function printT(table, level)
+	key = ""
+	local func = function(table, level)end
+	func = function(table, level)
+		level = level or 1
+		local indent = ""
+		for i = 1, level do
+			indent = indent.."  "
+		end
+
+		if key ~= "" then
+			print(indent..key.." ".."=".." ".."{")
+		else
+			print(indent .. "{")
+		end
+
+		key = ""
+		for k,v in pairs(table) do
+			if type(v) == "table" then
+				key = k
+				func(v, level + 1)
+			else
+				local content = string.format("%s%s = %s", indent .. "  ",tostring(k), tostring(v))
+				print(content)  
+			end
+		end
+		print(indent .. "}")
+	end
+	func(table, level)
+end
+
+
+-----------------------------------------------------------------------
+-- load and split data
+-----------------------------------------------------------------------
+function data_load(filename)
+	for line in io.lines(filename) do
+		local p1 = string.find(line, '|')
+		-- print(p1)
+	end
+end
+
+
+-----------------------------------------------------------------------
+-- testing case
+-----------------------------------------------------------------------
+data_load('c:/users/linwei/.fasd')
+local t = '123|aa|bb|cc||dd|'
+local r = string.split(t, '|')
+
+printT(r)
+
