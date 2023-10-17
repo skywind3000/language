022f2edaf8c8df5d57155531c1d9a13ca09eafcf
skywind3000 <skywind3000@163.com>
2023-10-17 16:54:09 +0800

    update range

---
+++ javascript/class_range2.js
@@ -0,0 +1,26 @@
+
+function Range(from, to) {
+	this.from = from;
+	this.to = to;
+}
+
+Range.prototype = {
+	includes: function (x) {
+		return this.from <= x && x <= this.to;
+	},
+	foreach: function (f) {
+		for (var x = Math.ceil(this.from); x <= this.to; x++) 
+			f(x);
+	},
+	toString: function () {
+		return"(" + this.from + "..." + this.to + ")";
+	},
+};
+
+var r = new Range(1, 3);
+
+console.log(r.includes(2));
+r.foreach(console.log);
+console.log(r);
+
+
