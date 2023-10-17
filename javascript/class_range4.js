acc74dabcf0d9c35fd4dd871e37c9aa0198af376
skywind3000 <skywind3000@163.com>
2023-10-17 21:18:33 +0800

    update range4

---
+++ javascript/class_range4.js
@@ -0,0 +1,37 @@
+
+function Range(from, to) {
+	this.from = from;
+	this.to = to;
+}
+
+Range.prototype.includes = function (x) {
+	return this.from <= x && x <= this.to;
+}
+
+Range.prototype.foreach = function (f) {
+	for (var x = Math.ceil(this.from); x <= this.to; x++) 
+		f(x);
+}
+
+Range.prototype.toString = function () {
+	return"(" + this.from + "..." + this.to + ")";
+}
+
+var r = Object.create(Range.prototype);
+Range.call(r, 1, 3);
+
+console.log(r.includes(2));
+r.foreach(console.log);
+console.log(r);
+
+console.log(typeof r);
+
+console.log(r instanceof Range);
+
+var R2 = function() {};
+R2.prototype = Range.prototype;
+
+console.log(r instanceof R2);
+
+
+
