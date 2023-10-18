41b8cfeb4f7ee2744ee8e2daa087c739f0f54150
skywind3000 <skywind3000@163.com>
2023-10-18 22:01:57 +0800

    update

---
+++ javascript/class_type.js
@@ -0,0 +1,30 @@
+
+function type(o) {
+	var t, c, n;
+	if (o === null) return "null";
+	if (!o == o) return 'nan';
+	if ((t = typeof o) !== 'object') return t;
+	if ((c = classof(o)) !== 'Object') return c;
+	if (o.constructor && typeof o.constructor === 'function'
+		&& (n = o.constructor.getName()))
+		return n;
+	return 'Object';
+}
+
+function classof(o) {
+	return Object.prototype.toString.call(o).slice(8, -1);
+}
+
+Function.prototype.getName = function () {
+	if ('name' in this) return this.name;
+	this.name = this.toString().match(/function\s*([^(]*)\(/)[1];
+	return this.name;
+}
+
+function Range(){}
+
+var r = new Range();
+
+console.log(type(r));
+
+
