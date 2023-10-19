2e95a1f9e3457ab8a75bc5e3a1f8c8413e0e3705
skywind3000 <skywind3000@163.com>
2023-10-20 00:09:21 +0800

    update module mjs

---
+++ javascript/module_export.mjs
@@ -0,0 +1,13 @@
+
+function foo(x, y) {
+	return x + y;
+}
+
+function bar(x, y) {
+	return x * y;
+}
+
+
+
+export { foo, bar };
+
---
+++ javascript/module_import.mjs
@@ -0,0 +1,6 @@
+
+import * as mod from './module_export.mjs'
+
+console.log(mod.foo(10, 20));
+console.log(mod.bar(10, 20));
+
