4cbf5831282ceeb6d06db2116340b01612763628
skywind3000 <skywind3000@163.com>
2023-10-19 16:31:58 +0800

    update boxing

--- javascript/object_boxing.js
+++ javascript/object_boxing.js
@@ -13,4 +13,9 @@ console.log(S.hello);
 
 console.log(s == S, s === S, s === S.valueOf());
 
+console.log(typeof s);
+console.log(typeof S);
+console.log(Object.prototype.toString.apply(s));
+console.log(Object.prototype.toString.apply(S));
+
 
