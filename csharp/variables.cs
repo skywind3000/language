06f5b81e34218e2a40598f7005cc6769dedbb31a
skywind3000 <skywind3000@163.com>
2023-11-17 14:29:44 +0800

    update datas

---
+++ csharp/variables.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Variables {
+	static void Main(string[] args) {
+		Console.WriteLine(sizeof(byte));
+		Console.WriteLine(sizeof(sbyte));
+		Console.WriteLine(sizeof(char));
+		Console.WriteLine(sizeof(short));
+		Console.WriteLine(sizeof(uint));
+		Console.WriteLine(sizeof(int));
+		Console.WriteLine(sizeof(UInt32));
+		Console.WriteLine(sizeof(Int16));
+	}
+}
+
+
