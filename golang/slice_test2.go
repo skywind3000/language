9cce9123b2b6fb02cd82a6eae25f517c1e131d1c
skywind3000 <skywind3000@163.com>
2024-03-12 18:03:41 +0800

    update slice_test2.go

--- golang/slice_test2.go
+++ golang/slice_test2.go
@@ -11,10 +11,16 @@ func main() {
 	a := []int{1, 2, 3}
 	b := append(a, 4)
 	c := b[:3]
-	d := append(b, 5)
+	d := append(c, 5)
 	displaySlice(a)
 	displaySlice(b)
 	displaySlice(c)
 	displaySlice(d)
 }
+
+// Output:
+// slice cap=3 len=3: [%!s(int=1) %!s(int=2) %!s(int=3)]
+// slice cap=6 len=4: [%!s(int=1) %!s(int=2) %!s(int=3) %!s(int=5)]
+// slice cap=6 len=3: [%!s(int=1) %!s(int=2) %!s(int=3)]
+// slice cap=6 len=4: [%!s(int=1) %!s(int=2) %!s(int=3) %!s(int=5)]
 
