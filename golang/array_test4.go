cc1b681b9870cc367f65d577c0384952cf538f1e
skywind3000 <skywind3000@163.com>
2024-03-22 17:35:41 +0800

    update golang/array_test4.go

--- golang/array_test4.go
+++ golang/array_test4.go
@@ -9,8 +9,15 @@ func foo(a [5]int) {
 	fmt.Println(a)
 }
 
+func bar(a *[5]int) {
+	a[0] = 200
+	fmt.Println(a)
+}
+
 func main() {
 	b := [...]int{1, 2, 3, 4, 5}
 	foo(b)
+	fmt.Println(b)
+	bar(&b)
 	fmt.Println(b)
 }
