e96fd6556d5474182cd4fb258362f745559b6ebf
skywind3000 <skywind3000@163.com>
2024-03-22 17:16:24 +0800

    update

--- golang/array_test1.go
+++ golang/array_test1.go
@@ -7,12 +7,11 @@ func main() {
 	b := a[1:2]
 	c := append(b, 9)
 	b[0] = 100
+	var d [5]int = [5]int{1: 100, 3: 200}
 	fmt.Println(a)
 	fmt.Println(cap(a))
 	fmt.Println(len(b))
 	fmt.Println(b)
 	fmt.Println(c)
+	fmt.Println(d)
 }
-
-
-
