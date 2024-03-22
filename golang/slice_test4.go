db1b30fccaf56ed080efb09ae39f87ecdaf4d070
skywind3000 <skywind3000@gmail.com>
2024-03-23 02:53:26 +0800

    Create new file slice_test4.go with printSlice and main functions. Add a functionality to print and modify slices.

---
+++ golang/slice_test4.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printSlice(slice []int) {
+	fmt.Printf("slice[%d/%d]: ", len(slice), cap(slice))
+	for _, v := range slice {
+		fmt.Printf("%d ", v)
+	}
+	fmt.Println()
+}
+
+func main() {
+	slice := []int{10, 20, 30, 40, 50}
+	newSlice := slice[1:3]
+	printSlice(slice)
+	printSlice(newSlice)
+	newSlice2 := append(newSlice, 60)
+	printSlice(slice)
+	printSlice(newSlice)
+	printSlice(newSlice2)
+
+}
