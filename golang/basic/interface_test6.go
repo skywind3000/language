568d2ba43e00eb96e6e18f4ff73208ed8c09444f
skywind3000 <skywind3000@163.com>
2024-03-26 16:43:14 +0800

    Add test cases for type assertions in interface_test6

---
+++ golang/basic/interface_test6.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func getInterface(what string) interface{} {
+	switch what {
+	case "int":
+		return (interface{})(1)
+	case "string":
+		return (interface{})("hello")
+	case "bool":
+		return (interface{})(true)
+	}
+	return nil
+}
+
+func main() {
+	var obj interface{}
+
+	obj = getInterface("int")
+	fmt.Printf("obj: %v, type: %T\n", obj, reflect.TypeOf(obj))
+
+	x, ok := obj.(int)
+	if ok {
+		fmt.Printf("value: %v\n", x)
+	}
+
+	obj = getInterface("string")
+	fmt.Printf("obj: %v, type: %T\n", obj, reflect.TypeOf(obj))
+
+	y, ok := obj.(string)
+	if ok {
+		fmt.Printf("value: %v\n", y)
+	}
+}
