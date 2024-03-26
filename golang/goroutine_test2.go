c1f633f11d6945ce073375a267afc7a0ceaf8e44
skywind3000 <skywind3000@163.com>
2024-03-26 17:31:11 +0800

    Add goroutine printing prime numbers for different prefixes

---
+++ golang/goroutine_test2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	wg.Add(2)
+
+	fmt.Println("Start Goroutines")
+	go printPrime("A")
+	go printPrime("B")
+
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
+
+	fmt.Println("\nTerminating Program")
+}
+
+func printPrime(prefix string) {
+	defer wg.Done()
+
+next:
+	for outer := 2; outer < 5000; outer++ {
+		for inner := 2; inner < outer; inner++ {
+			if outer%inner == 0 {
+				continue next
+			}
+		}
+		fmt.Printf("%s:%d\n", prefix, outer)
+	}
+	fmt.Println("Completed", prefix)
+}
