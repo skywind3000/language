5acbdd81f8ab3088a78bb1eb5c8b9d7e29582d9b
skywind3000 <skywind3000@gmail.com>
2024-03-27 00:14:25 +0800

    Add implementation for concurrent worker tasks using channels in Golang

---
+++ golang/channel_test3.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+const (
+	numberGoroutines = 4
+	taskLoad         = 10
+)
+
+var wg sync.WaitGroup
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	tasks := make(chan string, taskLoad)
+	wg.Add(numberGoroutines)
+	for gr := 1; gr <= numberGoroutines; gr++ {
+		go worker(tasks, gr)
+	}
+	for post := 1; post <= taskLoad; post++ {
+		tasks <- fmt.Sprintf("Task: %d", post)
+	}
+	close(tasks)
+	wg.Wait()
+}
+
+func worker(tasks chan string, worker int) {
+	defer wg.Done()
+	for {
+		task, ok := <-tasks
+		if !ok {
+			fmt.Printf("Worker: %d: Shutting Down\n", worker)
+			return
+		}
+		fmt.Printf("Worker: %d: Started %s\n", worker, task)
+		sleep := rand.Int63n(100)
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+		fmt.Printf("Worker: %d: Completed %s\n", worker, task)
+	}
+}
