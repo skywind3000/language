17160af6da457a55bc16f2aef7870f79bd6de62d
skywind3000 <skywind3000@163.com>
2024-03-29 17:17:15 +0800

    Add functionality to use a new package and print the result of a new function call.

--- golang/app/usepkg/use_package.go
+++ golang/app/usepkg/use_package.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 
 	"github.com/skywind3000/gosub"
+	dummy2 "github.com/skywind3000/gosub/dummy"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	x := gosub.DummySub(1, 2)
 	fmt.Println("1 + 2 =", x)
+	y := dummy2.DummyAdd2(9, 10)
+	fmt.Println("add2=", y)
 }
