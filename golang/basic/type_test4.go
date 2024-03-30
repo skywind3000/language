3af8a1de6bb8a3c58a1236000ea384d2b62807ed
skywind3000 <skywind3000@gmail.com>
2024-03-31 00:11:15 +0800

    Add multiplication method to Number type in golang/basic/type_test4.go
    - Added mul() method to Number type for multiplication functionality.
    - Tested multiplication operation in main function by creating a Number instance and invoking the new mul() method.

--- golang/basic/type_test4.go
+++ golang/basic/type_test4.go
@@ -10,8 +10,13 @@ func (self *Number) add(x Number) {
 	*self += x
 }
 
+func (self Number) mul(x Number) Number {
+	return self * x
+}
+
 func main() {
 	var x Number = 10
 	x.add(5)
 	fmt.Println("x = ", x)
+	fmt.Println(Number(10).mul(5))
 }
