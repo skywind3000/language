d13081b7474a4305d923b9f7911af71d260ca6bc
skywind3000 <skywind3000@gmail.com>
2024-04-01 03:14:13 +0800

    Add new test file foreign_test1.go for foreign package and update go.mod to include gosub as a replace and require dependency.

---
+++ golang/app/foreign/foreign_test1.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"skywind.com/golang/gosub/gosub"
+	"skywind.com/golang/gosub/lib"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	gosub.Foo()
+	lib.Bar()
+}
--- golang/app/foreign/go.mod
+++ golang/app/foreign/go.mod
@@ -1,3 +1,7 @@
 module skywind.com/golang/foreign
 
 go 1.22.1
+
+replace skywind.com/golang/gosub => ../gosub
+
+require skywind.com/golang/gosub v0.0.0-00010101000000-000000000000
