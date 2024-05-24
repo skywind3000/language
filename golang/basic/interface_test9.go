325ece1dc6f451c430fd0ddae9899116c36ab530
skywind3000 <skywind3000@163.com>
2024-05-24 15:02:20 +0800

    update type_check.go

--- golang/basic/interface_test9.go
+++ golang/basic/interface_test9.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func do(i interface{}) {
-	switch v := i.(type) {
-	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
-	default:
-		fmt.Printf("I don't know about type %T!\n", v)
+	if i == nil {
+		fmt.Printf("decode: obj is nil\n")
+	} else {
+		switch v := i.(type) {
+		case int:
+			fmt.Printf("Twice %v is %v\n", v, v*2)
+		case string:
+			fmt.Printf("%q is %v bytes long\n", v, len(v))
+		default:
+			fmt.Printf("I don't know about type %T!\n", v)
+		}
 	}
 }
 
@@ -17,4 +21,5 @@ func main() {
 	do(21)
 	do("hello")
 	do(true)
+	do(nil)
 }
