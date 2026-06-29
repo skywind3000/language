3d67d476b99d74feb180d3646605fe9d1ece4e49
skywind3000 <skywind3000@163.com>
2026-06-29 10:19:27 +0800

    update variable.cs

--- csharp/variables.cs
+++ csharp/variables.cs
@@ -1,5 +1,13 @@
 using System;
 
+namespace Asclib {
+	class Test {
+		public static void test() {
+			Console.WriteLine("NameSpace");
+		}
+	}
+}
+
 class Variables {
 	static void Main(string[] args) {
 		Console.WriteLine(sizeof(byte));
@@ -10,7 +18,13 @@ class Variables {
 		Console.WriteLine(sizeof(int));
 		Console.WriteLine(sizeof(UInt32));
 		Console.WriteLine(sizeof(Int16));
+		AscCore.Test.test();
 	}
 }
 
+// docmake.py
+// commandz.py
+
+// @command(build/win32): echo build1
+// @command(run):
 
